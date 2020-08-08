147817c8cc38b2409c3615203327f70958f5f04a
Andre Souza Alves <alves.andre.ds@gmail.com>
2020-08-08 18:24:09 -0300

    corrigindo erro

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -4,4 +4,9 @@ class Site::SearchController < SiteController
                          .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
                          .page(params[:page])
   end
+
+  def questions
+    @questions = Question. _search_(params[:page], params[:term])
+  end
+
 end
--- app/controllers/site/welcome_controller.rb
+++ app/controllers/site/welcome_controller.rb
@@ -1,7 +1,5 @@
 class Site::WelcomeController < SiteController
   def index
-    @questions = Question.includes(:answers)
-                         .order(:subject_id)
-                         .page(params[:page])
+    @questions = Question.last_questions(params[:page])
   end
 end
