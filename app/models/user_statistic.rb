31bab5ae4fafc1d125ea422d4762e6b87c7b17c0
Andre Souza Alves <alves.andre.ds@gmail.com>
2020-08-16 00:49:08 -0300

    refatorando code

--- app/models/user_statistic.rb
+++ app/models/user_statistic.rb
@@ -7,6 +7,19 @@ class UserStatistic < ApplicationRecord
   end
 
   def income
+    if total_questions != 0
      (self.right_question.to_f/self.total_questions.to_f)*100
+    else
+      0
+    end
+  end
+
+  #Class Methods
+  def self.set_statistic(answer, current_user)
+    if !!current_user
+      user_statistic = UserStatistic.find_or_create_by(user: current_user)
+      answer.correct? ? user_statistic.right_question += 1 : user_statistic.wrong_question +=1
+      user_statistic.save
+    end
   end
 end
