666040e7ce28643026a138b1a7948da3cbcc6072
Andre Souza Alves <alves.andre.ds@gmail.com>
2020-08-21 21:14:27 -0300

    Full-text Search no Rails

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -6,7 +6,7 @@ class Site::SearchController < SiteController
 
 
   def questions
-    @questions = Question._search_(params[:page], params[:term])
+    @questions = Question.search(params[:term], page: params[:page], per_page: 5)
   end
 
 end
